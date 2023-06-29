55512638088e77203d42d0d03ecf696998a292cb
leeda06 <88700168+leeda06@users.noreply.github.com>
2023-06-29 09:49:46 +0900

    파일 정리

--- 20230629_wsm/JS/data.js
+++ 20230629_wsm/JS/data.js
@@ -81,11 +81,11 @@ let reviews = [
     },
 ]
 
-let entertainer = ["아이유", "이석훈", "ZB1"]
-console.log(entertainer[2]);
+let entertainer = ["아이유", "이석훈", "zb1"]
+console.log(entertainer[2])
+let person = {"group": "zb1","name": "장하오", "country": "china"}
+console.log(person["name"]) // 장하오
+console.log(person.name) // 장하오
 
-let person = {"group": "ZB1", "name": "장하오", "country" : "china"}
-console.log(person["name"]);
-console.log(person.name);
-
-console.log(books[3].author);
+console.log(books[3].author) // 유연
+console.log(books[3]["author"]) // 유연
